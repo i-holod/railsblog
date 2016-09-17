7942c74df83dcee4998f08fd82c9bb4d6d28dfc1
HoLoD Sergey <holod@iholod.ru>
2016-09-18 00:05:04 +0300

    Static pages + pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
 
+  get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
+
   # или так get '/contacts' => 'contact#new'
 
   resource :contacts, only: [:new,:create], path_names: {:new => ''}
