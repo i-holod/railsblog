884ee9ca7f97b464ccae3b91d28b6700a6d2f070
HoLoD Sergey <holod@iholod.ru>
2016-09-17 21:31:05 +0300

    Articles show, edit, show all

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,40 @@ class ArticlesController < ApplicationController
   def new
   end
 
-  def create
-    render plain: params[:article].inspect
+  def edit
+    @article = Article.find(params[:id])
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @show_all = Article.order "created_at DESC"
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.valid?
+      @article.save
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    if @article.valid?
+      @article.save
+    else
+      render action: 'update'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title,:text)
+  end
 end
