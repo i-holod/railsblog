ce88a4ca6c262d82ce59a6e28e3defa1c4905714
HoLoD Sergey <holod@iholod.ru>
2016-09-17 18:57:06 +0300

    Change contacts form url

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  resource :contacts, only: [:new,:create]
+  # или так get '/contacts' => 'contact#new'
+
+  resource :contacts, only: [:new,:create], path_names: {:new => ''}
   resources :articles
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
